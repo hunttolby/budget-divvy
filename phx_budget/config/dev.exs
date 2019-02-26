7b1306b6ee24978e4404b8e2921c39454c2564e7
hunttolby <hunt_tolby@yahoo.com>
2019-02-25 20:14:02 -0700

    Updated info
    changed postgres password

--- phx_budget/config/dev.exs
+++ phx_budget/config/dev.exs
@@ -69,7 +69,7 @@ config :phoenix, :plug_init_mode, :runtime
 # Configure your database
 config :phx_budget, PhxBudget.Repo,
   username: "postgres",
-  password: "starWARS95",
+  password: "postgres",
   database: "phx_budget_dev",
   hostname: "localhost",
   pool_size: 10
